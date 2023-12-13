9247162499a70a9c9eba13ec3ed87f87fd148745
JFORNELLS <jfornells@yahoo.es>
2023-12-13 21:00:02 +0100

    Actualitzacio

--- src/ExTransferencies/1-RecibirEtherPayable.sol
+++ src/ExTransferencies/1-RecibirEtherPayable.sol
@@ -9,11 +9,13 @@ contract RecibirEtherPayable {
     error MaxAmountReached();
     error MaxBalanceReached();
 
+    uint256 public constant MIN_AMOUNT = 1 ether;
+    uint256 public constant MAX_AMOUNT = 10 ether;
     uint256 public balance;
 
     function recibirEther() external payable {
-        if (msg.value < 1 ether) revert MinAmountNotReached();
-        if (msg.value > 10 ether) revert MaxAmountReached();
+        if (msg.value < MIN_AMOUNT) revert MinAmountNotReached();
+        if (msg.value > MAX_AMOUNT) revert MaxAmountReached();
         balance += msg.value;
         if (balance > 50 ether) revert MaxBalanceReached();
         emit Deposited(msg.sender, msg.value);
